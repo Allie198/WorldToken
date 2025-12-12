f266a9b154ce9ec709792ed0a70a59904acbc422
Allie198 <alierenkara2006@gmail.com>
2025-12-12 23:37:13 +0300

    "feat: System.sol tamamlandı"

--- System.sol
+++ System.sol
@@ -1,63 +1,83 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
+
+import "./WorldToken.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-contract ETHPrice {
+contract System {
+
+    WorldToken public token;
 
     AggregatorV3Interface internal Gold_PriceFeed;
     AggregatorV3Interface internal ETH_PriceFeed;
     AggregatorV3Interface internal BTC_PriceFeed;
+    
+    mapping(address => uint256) public ethCollateral;
+    mapping(address => uint256) public mintedTokens;
 
-
-    constructor() {
-        Gold_PriceFeed = AggregatorV3Interface(0xC5981F461d74c46eB4b0CF3f4Ec79f025573B0Ea);
-        BTC_PriceFeed = AggregatorV3Interface (0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43);
-        ETH_PriceFeed = AggregatorV3Interface (0x694AA1769357215DE4FAC081bf1f309aDC325306);
+    uint256 public constant COLLATERAL_RATIO = 200;
+ 
+    constructor(address _tokenAddress) {
+        Gold_PriceFeed = AggregatorV3Interface (0xC5981F461d74c46eB4b0CF3f4Ec79f025573B0Ea);
+        BTC_PriceFeed  = AggregatorV3Interface (0x1b44F3514812d835EB1BDB0acB33d3fA3351Ee43);
+        ETH_PriceFeed  = AggregatorV3Interface (0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        token = WorldToken(_tokenAddress);
     }
 
-    function getLatestGoldPrice() public view returns (int256) {
-        (
-            /* uint80 roundID */,
-            int256 price,
-            /*uint startedAt*/,
-            /*uint timeStamp*/,
-            /*uint80 answeredInRound*/
-            
-        ) = Gold_PriceFeed.latestRoundData();
-        
-            return price;
+    function depositETH() external payable {
+        require(msg.value > 0, "ETH required");
+        ethCollateral[msg.sender] += msg.value;
     }
 
-    function getLatestETHPrice() public view returns (int256) {
-        (
-            /* uint80 roundID */,
-            int256 price,
-            /*uint startedAt*/,
-            /*uint timeStamp*/,
-            /*uint80 answeredInRound*/
-            
-        ) = ETH_PriceFeed.latestRoundData();
-        
-            return price;
+    function getPrice(AggregatorV3Interface feed) public view returns (uint256) {
+        (, int256 price, , , ) = feed.latestRoundData();
+        return uint256(price) * 1e10;
     }
 
-    function getLatestBTCPrice() public view returns (int256) {
-        (
-            /* uint80 roundID */,
-            int256 price,
-            /*uint startedAt*/,
-            /*uint timeStamp*/,
-            /*uint80 answeredInRound*/
-            
-        ) = BTC_PriceFeed.latestRoundData();
-        
-            return price;
+    function tokenPrice() public view returns(uint256) {
+         uint256 goldPrice = getPrice(Gold_PriceFeed);
+         uint256 ethPrice  = getPrice(ETH_PriceFeed);
+         uint256 btcPrice  = getPrice(BTC_PriceFeed);
+
+         return (goldPrice + ethPrice + btcPrice) / 20000;
     }
 
-    function mintToken(uint256 amountToMint) external { 
-           int256 goldPrice = getLatestGoldPrice();
-           int256 ethPrice = getLatestETHPrice();
-           int256 btcPrice = getLatestBTCPrice();
+    function mintToken(uint256 amount) external { 
+        uint256 ethPrice = getPrice(ETH_PriceFeed);
+        uint256 collateralValueUSD = (ethCollateral[msg.sender] * ethPrice) / 1e18;
+
+        require(collateralValueUSD > 0, "There is no colleteral");
+
+        uint256 currentTokenPrice = tokenPrice();
+        uint256 debtValueUSD = (mintedTokens[msg.sender] + amount) * currentTokenPrice / 1e18;
+
+        if (debtValueUSD > 0) {
+            require((collateralValueUSD * 100) / debtValueUSD >= COLLATERAL_RATIO, "Insuffucient Colleteral");
+        }
+
+        mintedTokens[msg.sender] += amount;
+        token.mint(msg.sender, amount);
+    
+    }
+
+    function burnToken(uint256 amount) external { 
+        require(mintedTokens[msg.sender] >= amount, "Unsufficient debt");
+
+        token.transferFrom(msg.sender, address(this), amount);
+        token.burn(address(this), amount);
+        mintedTokens[msg.sender] -= amount;
+
+        uint256 currentTokenPrice = tokenPrice();
+        uint256 ethPrice = getPrice(ETH_PriceFeed);
+
+        uint256 debtValueUSD = (amount * currentTokenPrice) / 1e18;
+        uint256 ethToReturn =  (debtValueUSD * 1e18) / ethPrice;
+
+        require(ethCollateral[msg.sender] >= ethToReturn, "Collateral error");
+        ethCollateral[msg.sender] -= ethToReturn;
+        payable(msg.sender).transfer(ethToReturn);
+
+
     }
 
     
